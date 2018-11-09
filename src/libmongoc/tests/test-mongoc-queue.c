42b488a4a80133f304cf64a9381614dcb895c2bc
samantharitter <samantha.ritter@10gen.com>
2018-11-09 16:28:47 -0500

    CDRIVER-2857 Implement a for_each method on mongoc queue type

--- src/libmongoc/tests/test-mongoc-queue.c
+++ src/libmongoc/tests/test-mongoc-queue.c
@@ -48,10 +48,68 @@ test_mongoc_queue_pop_tail (void)
    ASSERT_CMPVOID (_mongoc_queue_pop_tail (&q), ==, (void *) NULL);
 }
 
+static bool
+_queue_for_each_always_true (void *item, void *ctx)
+{
+   int *number = (int *) item;
+   *number = 1;
+   return true;
+}
+
+static bool
+_queue_for_each_modify_three_entries (void *item, void *ctx)
+{
+   int *number = (int *) item;
+   int *counter = (int *) ctx;
+
+   if (*counter > 2) {
+      return false;
+   }
+
+   (*counter)++;
+   *number = 1;
+
+   return true;
+}
+
+static void
+test_mongoc_queue_for_each (void)
+{
+   int i;
+   int counter;
+   int length = 5;
+   int numbers[5] = {0};
+   mongoc_queue_t q = MONGOC_QUEUE_INITIALIZER;
+
+   /* Sanity check for empty queues */
+   _mongoc_queue_for_each (&q, _queue_for_each_always_true, NULL);
+
+   /* Test that method modifies all entries when cb returns true */
+   for (i = 0; i < length; i++) {
+      _mongoc_queue_push_head (&q, &(numbers[i]));
+   }
+
+   _mongoc_queue_for_each (&q, _queue_for_each_always_true, NULL);
+   for (i = 0; i < length; i++) {
+      ASSERT (numbers[i] == 1);
+      numbers[i] = 0;
+   }
+
+   /* Test that method stops modifying when cb returns false */
+   counter = 0;
+   _mongoc_queue_for_each (&q, _queue_for_each_modify_three_entries, &counter);
+   counter = 0;
+   for (i = 0; i < length; i++) {
+      counter += numbers[i];
+   }
+
+   ASSERT (counter == 3);
+}
 
 void
 test_queue_install (TestSuite *suite)
 {
    TestSuite_Add (suite, "/Queue/basic", test_mongoc_queue_basic);
    TestSuite_Add (suite, "/Queue/pop_tail", test_mongoc_queue_pop_tail);
+   TestSuite_Add (suite, "/Queue/for_each", test_mongoc_queue_for_each);
 }
